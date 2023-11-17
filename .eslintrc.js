8b227a2f25b8dab36fa36e578f2ae06d2321fa9a
Ambuj Singh <97109381+that-ambuj@users.noreply.github.com>
2023-11-17 11:36:58 +0530

    chore: add package and tidy up nest and typescript configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,9 +17,29 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
+    quotes: ['error', 'double'],
+    '@typescript-eslint/no-floating-promises': 'error',
+    'require-await': 'off',
+    '@typescript-eslint/require-await': 'error',
+    '@typescript-eslint/no-misused-promises': 'error',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'function',
+        format: ['camelCase'],
+        leadingUnderscore: 'allow',
+      },
+      {
+        selector: 'variable',
+        format: ['snake_case', 'UPPER_CASE'],
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+    ],
   },
 };
