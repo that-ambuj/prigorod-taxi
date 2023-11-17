5b2b7c81104fe6f269fef2906024a7e72b66adfa
Ambuj Singh <97109381+that-ambuj@users.noreply.github.com>
2023-11-17 11:37:10 +0530

    formatting

--- src/otp.service.ts
+++ src/otp.service.ts
@@ -1,12 +1,12 @@
-import { Injectable } from '@nestjs/common';
-import { customAlphabet } from 'nanoid';
+import { Injectable } from "@nestjs/common";
+import { customAlphabet } from "nanoid";
 
 @Injectable()
 export class OtpService {
   private rng: () => string;
 
   constructor() {
-    this.rng = customAlphabet('1234567890', 6);
+    this.rng = customAlphabet("1234567890", 6);
   }
 
   public generateOtp(): string {
